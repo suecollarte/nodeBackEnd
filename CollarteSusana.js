ed65cf1fe479ba6d135880dc4cc57c1af7fc04de
suecollarte <suecollarte@gmail.com>
2022-07-25 12:57:25 -0400

    Add files via upload
    Curso javacript tarea 1

--- CollarteSusana.js
+++ CollarteSusana.js
@@ -1,52 +1,46 @@
 class Usuario {
-    constructor(nombre,apellido) {
-        this.nombre= nombre;
-        this.apellido=apellido;
-        this.Libros =[];
-        this.Mascotas=[];
-       
+    constructor (nombre,apellido,libros,mascotas){
+        this.nombre=nombre
+        this.apellido=apellido
+        this.libros=[]
+        this.mascotas=[]
+        this.cant=0
     }
-
-    get getFullName(){
-        return this.FullName;
-    }
-    FullName(){
-        console.log( "Usuario Nombre Completo: "+this.nombre +" "+ this.apellido);
+    
+    
+    getfullName(){
+        return this.nombre+this.apellido
     }
     
     countMascotas(){
-  
-        console.log(this.Mascotas);
-        console.log("Hay "+this.Mascotas.length+" mascotas");
-        }
-    addMascotas(tipoMascota){
-        this.Mascotas.push(tipoMascota);
-        
+        return this.cant;
     }
-        get getBookNames(){
-            return this.BookNames();
-            //console.log(Libros[2]);
-         }
-     BookNames(){
-        let nombreLibros=this.Libros.map(libroItem =>{return libroItem});
-        console.log(nombreLibros);
-     }
-     addBook(nombre,autor){
-        this.Libros.push({nombre,autor});
-        console.log(this.Libros);
-     }
-}
-
-const persona1= new Usuario ("Susana", "Collarte") ;
+    addMascota(mascota){
+        
+        this.mascotas.push(mascota);
+        this.cant =this.cant+1;
     
-persona1.addMascotas("perro");
-persona1.addMascotas("gato");
-persona1.addMascotas("gato"); 
-persona1.countMascotas();
-
-persona1.FullName();
-persona1.BookNames();
-
-/* addBook*/
-persona1.addBook("señor de los anillos", "JRR Tolkien")
-persona1.BookNames();
+    }
+    addBook(nombre,autor){
+        
+           
+        this.libros.push({nombre,autor});
+    
+    }
+    
+    getBookNames(){
+        return this.BookNames();
+    }
+    BookNames(){
+       let l_Libro= this.libros;
+        let nombreLibros=l_Libro.map(function(obj){
+            //let robj ={};
+            //robj[obj.nombre]=obj.autor;
+        //return robj;
+        return obj.nombre;
+    });
+    return nombreLibros ;
+    //return this.libros;
+    }
+    
+    }
